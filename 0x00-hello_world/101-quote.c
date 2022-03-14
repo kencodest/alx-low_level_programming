e883e10ab533d501a406c05efb146873fb84bb82
kencodest <muiaken88@gmail.com>
2022-03-14 15:19:28 -0700

    add 101-quote.c edit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - prints "and that piece of art is useful - Dora Korpar, 2015--10-19"
  * followed by a anew line to the standard error
